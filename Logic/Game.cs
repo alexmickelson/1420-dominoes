dd860f73074a6e06f5d641f2df03a95410a9589d
Alex Mickelson <alexmickelson96@gmail.com>
2024-11-11 15:22:15 -0700

    can create games, cannot see created games dynamically

---
+++ Logic/Game.cs
@@ -0,0 +1,10 @@
+public class Game
+{
+  public GameStatus Status { get; private set; }
+  public string Name { get; set; }
+  public int NumberOfPlayers { get; set; }
+  public Game()
+  {
+    Status = GameStatus.NOT_STARTED;
+  }
+}

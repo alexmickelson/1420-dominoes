f160a5db21c2185d79378d15cfdd0eb3d77e20e7
Alex Mickelson <alexmickelson96@gmail.com>
2024-11-14 14:29:29 -0700

    updates

--- Logic/Lobby.cs
+++ Logic/Lobby.cs
@@ -2,4 +2,9 @@ public static class Lobby
 {
   public static List<Game> Games { get; } = new List<Game>();
   public static event Action? GameListChanged;
+
+  public static void RunGameListChangedEvent()
+  {
+    GameListChanged?.Invoke();
+  }
 }

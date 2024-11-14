dc8dd4c740ca16f797d061e60b2b4867b36cff03
Alex Mickelson <alex.mickelson@snow.edu>
2024-11-14 15:04:33 -0700

    added game and player

--- Logic/Game.cs
+++ Logic/Game.cs
@@ -1,8 +1,26 @@
 public class Game
 {
+  public static Game Instance { get; private set; } = new Game();
   public GameStatus Status { get; private set; }
   public string Name { get; set; }
   public int NumberOfPlayers { get; set; }
+
+  public Player Player1 { get; private set; }
+  public Player Player2 { get; private set; }
+
+  public List<Tile> Board { get; private set; }
+  public bool NoOneCanPlay
+  {
+    get
+    {
+      if (Player1 is null || Player2 is null)
+        return false;
+
+      var player1CanPlay = Player1.HasMatchFor(Board.Last());
+      var player2CanPlay = Player2.HasMatchFor(Board.Last());
+      return !player1CanPlay && !player2CanPlay;
+    }
+  }
   public Game()
   {
     Status = GameStatus.NOT_STARTED;
